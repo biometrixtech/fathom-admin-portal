a110d1a6ef3385aae35e571a9a959b3ddcc60b79
Mazen Chami <mazenchami@gmail.com>
2018-11-06 15:19:30 -0500

    fixing comments

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@
 */
 
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistReducer, persistStore, } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 import rootReducer from './reducers/rootReducer';
 
